2272edfc1cd254023530fec669aabbd475e278a5
Juan Pablo Hurtado <juanpablo.hz02@gmail.com>
2023-04-30 11:03:53 -0500

    insertar muchos pokemons por mi cuenta

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interface/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
   private readonly axios: AxiosInstance = axios;
   async executeSeed() {
     const { data } = await axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
+    const pokemons: CreatePokemonDto[] = [];
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
       console.log({ name, no });
+      pokemons.push({ name, no });
     });
 
+    await this.pokemonModel.insertMany(pokemons);
     return data;
   }
 }
