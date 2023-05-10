5cf7ff37636f26cc326cfafa307a77d40fcf4ce5
Juan Pablo Hurtado <juanpablo.hz02@gmail.com>
2023-05-09 21:58:16 -0500

    default port

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api/v2');
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on port: ${process.env.PORT}`);
 }
 bootstrap();
